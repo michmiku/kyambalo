e299f5e976cf770e9618a08f056143532e63bb69
michal <michmiku@gmail.com>
2020-11-09 15:50:55 +0100

    iamge hover animation

--- components/images.tsx
+++ components/images.tsx
@@ -5,13 +5,12 @@ export default function Images({ files, path, handleClick }) {
   return (
     <div className={styles.grid}>
       {files.map((image, key) => (
-        <div key={key}>
+        <div key={key} onClick={(e) => handleClick(image, path, true, e)}>
           <Image
             src={path + image.fileName}
             alt={image}
             width={300}
             height={230}
-            onClick={(e) => handleClick(image, path, true, e)}
           />
         </div>
       ))}
