3e08e63bc447fece82a74877ed3e54a56cc7dd3a
michal <michmiku@gmail.com>
2020-11-11 20:06:09 +0100

    cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,36 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
-import "semantic-ui-css/semantic.min.css";
+import CookieConsent from "react-cookie-consent";
+import Link from "next/link";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   <Head>
     <meta name="viewport" content="viewport-fit=cover" />
   </Head>;
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <CookieConsent
+        location="bottom"
+        buttonText="WYRAŻAM ZGODĘ"
+        cookieName="cookies"
+        style={{ background: "#2B373B", fontSize: "0.7em" }}
+        buttonStyle={{
+          color: "white",
+          fontSize: "0.7em",
+          background: "rgb(150, 97, 61)",
+        }}
+      >
+        Wykorzystujemy pliki cookies („ciasteczka”) własne i firm zewnętrznych,
+        analizując sposób użytkowania naszego portalu, w celu ulepszenia naszych
+        usług i ułatwienia Państwu korzystanie z nich. Dalsze korzystanie z tej
+        witryny oznacza akceptację plików cookies. Więcej informacji można
+        znaleźć w dziale{" "}
+        <Link href="/polityka-prywatnosci">„Informacja o plikach cookies”</Link>{" "}
+      </CookieConsent>
+    </React.Fragment>
+  );
 };
 
 export default MyApp;
