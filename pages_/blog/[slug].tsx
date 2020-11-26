468191cd9ab9956d722a63ed786551f725735736
michal <michmiku@gmail.com>
2020-11-26 23:13:10 +0100

    fix bug

--- pages_/blog/[slug].tsx
+++ pages_/blog/[slug].tsx
@@ -65,11 +65,13 @@ const PostPage = ({ post }) => {
         />
         {console.log(post.feature_image)}
         <meta property="og:url" content="https://kyambalo.com/blog/" />
-        <meta
-          property="og:image"
-          content={post !== undefined ? post.feature_image : null}
-          key="ogimage"
-        />
+        {post !== undefined ? (
+          <meta
+            property="og:image"
+            content={post.feature_image}
+            key="ogimage"
+          />
+        ) : null}
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:description"
