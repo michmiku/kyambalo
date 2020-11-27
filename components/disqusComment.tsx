e460f9441820c9cc16efb1c586765cdcd88b64e1
michal <michmiku@gmail.com>
2020-11-27 20:41:10 +0100

    fixed disqus?

--- components/disqusComment.tsx
+++ components/disqusComment.tsx
@@ -3,8 +3,8 @@ const DisqusComments = ({ post }) => {
   const disqusShortname = "kyambalo";
   const disqusConfig = {
     url: `https://kyambalo.com/blog/${post !== undefined ? post.slug : null}`,
-    identifier: post.slug, // Single post id
-    title: post.slug, // Single post title
+    identifier: `${post !== undefined ? post.slug : null}`, // Single post id
+    title: `${post !== undefined ? post.slug : null}`, // Single post title
   };
   return (
     <div>
